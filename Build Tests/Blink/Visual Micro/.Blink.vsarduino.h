b679bcb424bb7164e3956eed96b1560f5e15803d
jcheifetz <jonathan.cheifetz@gmail.com>
2013-10-08 20:24:35 +0100

    V1

---
+++ Build Tests/Blink/Visual Micro/.Blink.vsarduino.h
@@ -0,0 +1,13 @@
+//Board = Arduino Uno
+#define ARDUINO 105
+#define __AVR_ATmega328P__
+#define F_CPU 16000000L
+#define __AVR__
+extern "C" void __cxa_pure_virtual() {;}
+
+//already defined in arduno.h
+//already defined in arduno.h
+
+#include "C:\Program Files\Arduino\hardware\arduino\variants\standard\pins_arduino.h" 
+#include "C:\Program Files\Arduino\hardware\arduino\cores\arduino\arduino.h"
+#include "C:\Users\cheifetz\Desktop\PWM\AC\Repo\Build Tests\Blink\Blink.ino"
