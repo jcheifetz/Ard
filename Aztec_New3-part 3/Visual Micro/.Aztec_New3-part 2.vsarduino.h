75db66100fcbb29511a201880233d552db8028dc
jcheifetz <jonathan.cheifetz@gmail.com>
2014-04-23 19:45:44 +0100

    ha

---
+++ Aztec_New3-part 3/Visual Micro/.Aztec_New3-part 2.vsarduino.h
@@ -0,0 +1,14 @@
+//Board = Arduino Uno
+#define ARDUINO 105
+#define __AVR_ATmega328P__
+#define F_CPU 16000000L
+#define __AVR__
+extern "C" void __cxa_pure_virtual() {;}
+
+//already defined in arduno.h
+//already defined in arduno.h
+void zeroCross();
+
+#include "C:\Program Files\Arduino\hardware\arduino\variants\standard\pins_arduino.h" 
+#include "C:\Program Files\Arduino\hardware\arduino\cores\arduino\arduino.h"
+#include "C:\Users\cheifetz\Desktop\PWM\AC\Repo\Aztec_New3-part 2\Aztec_New3.ino"
