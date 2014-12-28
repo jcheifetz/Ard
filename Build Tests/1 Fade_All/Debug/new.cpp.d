e5ee513d26b07e1b2e75cf0c29242a0f17c03505
jcheifetz <jonathan.cheifetz@gmail.com>
2014-12-28 15:54:34 +0000

    WR2

---
+++ Build Tests/1 Fade_All/Debug/new.cpp.d
@@ -0,0 +1,4 @@
+ \
+ C:\Users\EliteBook\AppData\Local\VMicro\Arduino\Builds\Fade_All\uno\new.cpp.o:  \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\cores\arduino\new.cpp \
+  C:\Program\ Files\ (x86)\Arduino\hardware\arduino\cores\arduino/new.h
