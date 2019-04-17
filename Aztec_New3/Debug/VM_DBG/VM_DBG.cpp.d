300fd25bd4906c8a4742a64dc0d467a1a495d034
Jonathan <jonathan.cheifetz@gmail.com>
2019-04-17 15:27:41 +0100

    Start a3

---
+++ Aztec_New3/Debug/VM_DBG/VM_DBG.cpp.d
@@ -0,0 +1,14 @@
+F:\Users\cheifetz\AppData\Local\Temp\VMBuilds\Aztec_New3\uno\Debug\VM_DBG\VM_DBG.cpp.o: \
+ f:\program\ files\ (x86)\microsoft\ visual\ studio\2019\community\common7\ide\extensions\pnjvqy5x.ql4\Micro\ Platforms\default\debuggers\VM_DBG\VM_DBG.cpp \
+ f:\program\ files\ (x86)\microsoft\ visual\ studio\2019\community\common7\ide\extensions\pnjvqy5x.ql4\Micro\ Platforms\default\debuggers\VM_DBG\VM_DBG.h \
+ F:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Arduino.h \
+ F:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/binary.h \
+ F:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/WCharacter.h \
+ F:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/WString.h \
+ F:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/HardwareSerial.h \
+ F:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Stream.h \
+ F:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Print.h \
+ F:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Printable.h \
+ F:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/USBAPI.h \
+ F:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Arduino.h \
+ F:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\variants\standard/pins_arduino.h
---
+++ Aztec_New3/Debug/VM_DBG/VM_mem_check_sam.cpp.d
@@ -0,0 +1,2 @@
+F:\Users\cheifetz\AppData\Local\Temp\VMBuilds\Aztec_New3\uno\Debug\VM_DBG\VM_mem_check_sam.cpp.o: \
+ f:\program\ files\ (x86)\microsoft\ visual\ studio\2019\community\common7\ide\extensions\pnjvqy5x.ql4\Micro\ Platforms\default\debuggers\VM_DBG\VM_mem_check_sam.cpp
