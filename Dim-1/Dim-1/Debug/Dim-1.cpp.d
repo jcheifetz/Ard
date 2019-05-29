c17b751b9c4afbc42e3b4c2494191d28eaca823c
Jonathan <jonathan.cheifetz@gmail.com>
2019-05-29 12:07:02 +0100

    Yellow dim

---
+++ Dim-1/Dim-1/Debug/Dim-1.cpp.d
@@ -0,0 +1,14 @@
+F:\Users\cheifetz\AppData\Local\Temp\VMBuilds\Dim-1\uno\Debug\Dim-1.cpp.o: \
+ F:\Users\cheifetz\AppData\Local\Temp\VMBuilds\Dim-1\uno\Debug\Dim-1.cpp \
+ F:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/arduino.h \
+ F:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/binary.h \
+ F:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/WCharacter.h \
+ F:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/WString.h \
+ F:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/HardwareSerial.h \
+ F:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Stream.h \
+ F:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Print.h \
+ F:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Printable.h \
+ F:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/USBAPI.h \
+ F:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Arduino.h \
+ F:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\variants\standard/pins_arduino.h \
+ F:\Users\cheifetz\Documents\Arduino\libraries\digitalWriteFast/digitalWriteFast.h
